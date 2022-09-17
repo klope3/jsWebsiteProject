61f557a5baae52910bcb3f96fb31527d867c4b38
Josh Klope <klope3@msn.com>
2022-09-16 21:21:10 -0500

    Add logic to build items programmatically; code cleanup

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//#region Classes
 class FakeObject
 {
     constructor(name, size, color, isPopular)
@@ -8,8 +9,9 @@ class FakeObject
         this.isPopular = isPopular;
     }
 }
-
-const objects = 
+//#endregion
+//#region Variables & Constants
+const dataObjects = 
 [
     new FakeObject("Item 1", "15", "Red", "No"),
     new FakeObject("Item 2", "132", "Green", "Yes"),
@@ -20,32 +22,60 @@ const objects =
     new FakeObject("Item 7", "12", "Black", "No"),
     new FakeObject("Item 8", "6", "Green", "No"),
 ];
-const testItem = document.querySelector("#test-item");
-const items = document.querySelectorAll(".item");
 const collectionMain = document.querySelector("#collection-main");
 const collectionFavorites = document.querySelector("#collection-favorites");
-const rect = testItem.getBoundingClientRect();
-
-for (let item of items)
+const items = buildCollectionMain();
+//#endregion
+//#region Functions
+function buildCollectionMain()
 {
-    item.addEventListener("click", clickEvent => moveClickedItem(clickEvent));
-}
-
-
-
-buildCollection();
-
-function buildCollection()
-{
-    for (let obj of objects)
+    let newItems = [];
+    for (let obj of dataObjects)
     {
-
+        let itemFromData = buildItem(obj);
+        collectionMain.appendChild(itemFromData);
+        newItems.push(itemFromData);
     }
+    return newItems;
 }
 
 function buildItem(fakeObject)
 {
+    let itemDiv = document.createElement("div");
+    itemDiv.classList.add("item");
+    itemDiv.addEventListener("click", clickEvent => moveClickedItem(clickEvent));
 
+    let h2 = document.createElement("h2");
+    let h2Text = document.createTextNode(fakeObject.name);
+    h2.appendChild(h2Text);
+    itemDiv.appendChild(h2);
+
+    let ul = document.createElement("ul");
+    itemDiv.appendChild(ul);
+
+    ul.appendChild(buildItemDataRow("Name:", fakeObject.name));
+    ul.appendChild(buildItemDataRow("Size:", fakeObject.size));
+    ul.appendChild(buildItemDataRow("Color:", fakeObject.color));
+    ul.appendChild(buildItemDataRow("Is Popular:", fakeObject.isPopular));
+
+    return itemDiv;
+}
+
+function buildItemDataRow(rowLabel, rowValue)
+{
+    let li = document.createElement("li");
+    
+    let strong = document.createElement("strong");
+    let strongText = document.createTextNode(rowLabel);
+    strong.appendChild(strongText);
+    li.appendChild(strong);
+
+    let span = document.createElement("span");
+    let spanText = document.createTextNode(rowValue);
+    span.appendChild(spanText);
+    li.appendChild(span);
+
+    return li;
 }
 
 function moveClickedItem(clickEvent)
@@ -66,3 +96,4 @@ function moveClickedItem(clickEvent)
         target.style.transform = "translate(0)";
     }, 1);
 }
+//#endregion
