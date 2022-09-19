fcd6885551c5120f2f9e61b142b1efeff3394d29
Josh Klope <klope3@msn.com>
2022-09-19 11:12:35 -0500

    Add mass sums

--- main.js
+++ main.js
@@ -1,42 +1,112 @@
+//#region Classes
+class Collection 
+{
+    constructor(listDivID)
+    {
+        this.listDiv = document.querySelector(`#${listDivID}`);
+        this.reverseSort = false;
+        this.sumDiv = document.querySelector(`#${listDivID}-sum`);
+        this.numberSpan = document.createElement("span");
+        this.superscript = document.createElement("sup");
+        this.unitSpan = document.createElement("span");
+        this.numberSpan.textContent = "0";
+        this.unitSpan.textContent = " kg";
+        this.sumDiv.appendChild(this.numberSpan);
+        this.sumDiv.appendChild(this.superscript);
+        this.sumDiv.appendChild(this.unitSpan);
+        //this.asteroids = [];
+        this.massSum = new SciNotNumber(0, 0);
+    }
+
+    invertSortSetting = () => this.reverseSort = !this.reverseSort;
+
+    addMass(mass, multiplier = 1)
+    {
+        if (mass === null) { return; }
+        let num = new SciNotNumber(mass.massValue * multiplier, mass.massExponent);
+        this.massSum.add(num);
+    }
+
+    updateSumText()
+    {
+        let roundedMass = new SciNotNumber(this.massSum.coefficient, this.massSum.exponent);
+        roundedMass.coefficient = roundedMass.coefficient.toFixed(2);
+        let isZero = roundedMass.coefficient <= 0 || roundedMass.exponent < 5; //deal with imprecision by throwing out lower numbers
+        this.numberSpan.textContent = isZero ? "0" : `${roundedMass.coefficient}x10`;
+        this.superscript.textContent = isZero ? "" : "" + roundedMass.exponent;
+    }
+    //addMass = (mass) => this.massSum.add(new SciNotNumber(mass.))
+}
+
+class SciNotNumber 
+{
+    constructor(coefficient, exponent)
+    {
+        this.coefficient = coefficient;
+        this.exponent = exponent;
+    }
+
+    toString = () => `${this.coefficient}x10^${this.exponent}`;
+
+    add(sciNotNumber)
+    {
+        let {coefficient: otherCoefficient, exponent: otherExponent} = sciNotNumber;
+
+        let incrementor = this.exponent < otherExponent ? -1 : 1;
+        let multiplier = this.exponent < otherExponent ? 10 : 0.1;
+        while (this.exponent != otherExponent)
+        {
+            otherExponent += incrementor;
+            otherCoefficient *= multiplier;
+        }
+    
+        this.coefficient += otherCoefficient;
+        while (this.coefficient > 10)
+        {
+            this.coefficient /= 10;
+            this.exponent++;
+        }
+        let safety = 0;
+        while (this.coefficient > 0 && this.coefficient < 1 && safety < 9999)
+        {
+            this.coefficient *= 10;
+            this.exponent--;
+            safety++;
+        }
+        if (safety === 9999) { console.log("ERROR: Runaway loop"); }
+    }
+}
+//#endregion
 //#region Variables & Constants
-const collectionMain = document.querySelector("#collection-main");
-const collectionFavorites = document.querySelector("#collection-favorites");
+const collectionMain = new Collection("collection-main");
+const collectionFavorites = new Collection("collection-favorites");
 const collectionMainSort = document.querySelector("#collectionMainSort");
 const collectionFavSort = document.querySelector("#collectionFavSort");
-const collectionMainSum = document.querySelector("#collectionMainSum");
-const collectionFavSum = document.querySelector("#collectionFavSum");
-const asteroids = new Map();
-const sortSettings = new Map([
-    [collectionMain, false],
-    [collectionFavorites, false],
-])
-let reverseMain = true;
-let reverseFavs = false;
+const cardObjects = new Map();
 //#endregion
 //#region Execution
 fetch("https://api.le-systeme-solaire.net/rest/bodies?filter[]=bodyType,eq,asteroid&filter[]=meanRadius,gt,0").then(response => response.json()).then(data => buildCollectionMain(data));
 collectionMainSort.addEventListener("click", () => {
-    let curSetting = sortSettings.get(collectionMain);
-    sortSettings.set(collectionMain, !curSetting);
+    collectionMain.invertSortSetting();
     sortCollection(collectionMain);
 });
 collectionFavSort.addEventListener("click", () => {
-    let curSetting = sortSettings.get(collectionFavorites);
-    sortSettings.set(collectionFavorites, !curSetting);
+    collectionFavorites.invertSortSetting();
     sortCollection(collectionFavorites);
 });
 //endregion
 //#region Functions
 function buildCollectionMain(data)
 {
-    console.log(data);
     for (let ast of data.bodies)
     {
         let builtEntry = buildEntry(ast);
-        asteroids.set(builtEntry, ast);
-        collectionMain.appendChild(builtEntry);
+        collectionMain.listDiv.appendChild(builtEntry);
+        cardObjects.set(builtEntry, ast);
+        collectionMain.addMass(ast.mass);
+        //collectionMain.asteroids.push(ast);
     }
-    console.log(asteroids);
+    collectionMain.updateSumText();
     sortCollection(collectionMain);
 }
 
@@ -87,38 +157,47 @@ function buildItemDataRow(rowLabel, rowValue)
 
 function moveClickedItem(clickEvent)
 {
-    let target = clickEvent.target;
-    target.classList.remove("transition");
-    let curParent = target.parentElement;
-    let newParent = curParent === collectionMain ? collectionFavorites : collectionMain;
-    let startRect = target.getBoundingClientRect();
-    newParent.appendChild(target)
-    sortCollection(newParent);
-    updateSums();
-    let endRect = target.getBoundingClientRect();
+    let asteroidCard = clickEvent.target;
+    asteroidCard.classList.remove("transition");
+    let prevParent = asteroidCard.parentElement;
+    let prevParentCollection = prevParent === collectionMain.listDiv ? collectionMain : collectionFavorites;
+    let newParentCollection = prevParent === collectionMain.listDiv ? collectionFavorites : collectionMain;
+    let startRect = asteroidCard.getBoundingClientRect();
+    newParentCollection.listDiv.appendChild(asteroidCard)
+    sortCollection(newParentCollection);
+    let astObject = cardObjects.get(asteroidCard);
+    let astMass = astObject.mass;
+    prevParentCollection.addMass(astMass, -1);
+    prevParentCollection.updateSumText();
+    newParentCollection.addMass(astMass);
+    newParentCollection.updateSumText();
+    let endRect = asteroidCard.getBoundingClientRect();
     let differenceX = startRect.x - endRect.x;
     let differenceY = startRect.y - endRect.y;
-    target.style.transform = `translate(${differenceX}px, ${differenceY}px)`;
+    asteroidCard.style.transform = `translate(${differenceX}px, ${differenceY}px)`;
     setTimeout(() => {
-        target.classList.add("transition");
-        target.style.transform = "translate(0)";
+        asteroidCard.classList.add("transition");
+        asteroidCard.style.transform = "translate(0)";
     }, 1);
 }
 
 function updateSums()
 {
+    let mainSum = collectionMain.asteroids.reduce((accumulator, item) => {
 
+    })
 }
 
 function sortCollection(targetCollection)
 {
-    let children = targetCollection.childNodes;
+    let div = targetCollection.listDiv;
+    let children = div.childNodes;
     let childrenArr = Array.from(children);
-    let reverse = sortSettings.get(targetCollection);
+    let reverse = targetCollection.reverseSort;
     childrenArr.sort((a, b) => compareStrings(a.dataset.astName, b.dataset.astName, reverse));
     for (let child of childrenArr)
     {
-        targetCollection.appendChild(child);
+        div.appendChild(child);
     }
 }
 
