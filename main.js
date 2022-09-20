fe078b8cc059785cf079041f71d7e2be70208bf9
Josh Klope <klope3@msn.com>
2022-09-20 09:57:45 -0500

    Refactor and clean up code; fix bug with empty collections having nonzero mass

--- main.js
+++ main.js
@@ -3,9 +3,14 @@ class Collection
 {
     constructor(listDivID)
     {
+        //find elements, assign default values
         this.listDiv = document.querySelector(`#${listDivID}`);
-        this.reverseSort = false;
         this.sumDiv = document.querySelector(`#${listDivID}-sum`);
+        this.sortIcon = document.querySelector(`#${listDivID}-sort-icon`);
+        this.massSum = new SciNotNumber(0, 0);
+        this.totalEntries = 0;
+        this.reverseSort = false;
+        //create and setup elements for correctly formatted scientific notation
         this.numberSpan = document.createElement("span");
         this.superscript = document.createElement("sup");
         this.unitSpan = document.createElement("span");
@@ -14,16 +19,28 @@ class Collection
         this.sumDiv.appendChild(this.numberSpan);
         this.sumDiv.appendChild(this.superscript);
         this.sumDiv.appendChild(this.unitSpan);
-        //this.asteroids = [];
-        this.massSum = new SciNotNumber(0, 0);
     }
 
-    invertSortSetting = () => this.reverseSort = !this.reverseSort;
+    invertSortSetting = () => {
+        this.reverseSort = !this.reverseSort
+        this.updateSortIcon();
+    };
+
+    updateSortIcon()
+    {
+        let normalIconClass = "fa-arrow-down-a-z";
+        let reversedIconClass = "fa-arrow-down-z-a";
+        let classToAdd = this.reverseSort ? reversedIconClass : normalIconClass;
+        this.sortIcon.classList.remove(normalIconClass);
+        this.sortIcon.classList.remove(reversedIconClass);
+        this.sortIcon.classList.add(classToAdd);
+    }
 
     addMass(mass, multiplier = 1)
     {
         if (mass === null) { return; }
         let num = new SciNotNumber(mass.massValue * multiplier, mass.massExponent);
+        this.totalEntries += multiplier; //multiplier should only ever be 1 or -1
         this.massSum.add(num);
     }
 
@@ -31,7 +48,7 @@ class Collection
     {
         let roundedMass = new SciNotNumber(this.massSum.coefficient, this.massSum.exponent);
         roundedMass.coefficient = roundedMass.coefficient.toFixed(2);
-        let isZero = roundedMass.coefficient <= 0 || roundedMass.exponent < 5; //deal with imprecision by throwing out lower numbers
+        let isZero = this.totalEntries === 0;
         this.numberSpan.textContent = isZero ? "0" : `${roundedMass.coefficient}x10`;
         this.superscript.textContent = isZero ? "" : "" + roundedMass.exponent;
     }
@@ -51,7 +68,7 @@ class SciNotNumber
     add(sciNotNumber)
     {
         let {coefficient: otherCoefficient, exponent: otherExponent} = sciNotNumber;
-
+        //convert other number so it has the same exponent
         let incrementor = this.exponent < otherExponent ? -1 : 1;
         let multiplier = this.exponent < otherExponent ? 10 : 0.1;
         while (this.exponent != otherExponent)
@@ -61,27 +78,25 @@ class SciNotNumber
         }
     
         this.coefficient += otherCoefficient;
+        //convert result to correct sci. not. form (coefficient within range 1-10)
         while (this.coefficient > 10)
         {
             this.coefficient /= 10;
             this.exponent++;
         }
-        let safety = 0;
-        while (this.coefficient > 0 && this.coefficient < 1 && safety < 9999)
+        while (this.coefficient > 0 && this.coefficient < 1)
         {
             this.coefficient *= 10;
             this.exponent--;
-            safety++;
         }
-        if (safety === 9999) { console.log("ERROR: Runaway loop"); }
     }
 }
 //#endregion
 //#region Variables & Constants
 const collectionMain = new Collection("collection-main");
 const collectionFavorites = new Collection("collection-favorites");
-const collectionMainSort = document.querySelector("#collectionMainSort");
-const collectionFavSort = document.querySelector("#collectionFavSort");
+const collectionMainSort = document.querySelector("#collection-main-sort");
+const collectionFavSort = document.querySelector("#collection-fav-sort");
 const cardObjects = new Map();
 //#endregion
 //#region Execution
@@ -89,14 +104,9 @@ fetch("https://api.le-systeme-solaire.net/rest/bodies?filter[]=bodyType,eq,aster
     document.querySelector("#loading-spinner").style.display = "none";
     buildCollectionMain(data);
 });
-collectionMainSort.addEventListener("click", () => {
-    collectionMain.invertSortSetting();
-    sortCollection(collectionMain);
-});
-collectionFavSort.addEventListener("click", () => {
-    collectionFavorites.invertSortSetting();
-    sortCollection(collectionFavorites);
-});
+
+setupSortButtonFor(collectionMain);
+setupSortButtonFor(collectionFavorites);
 //endregion
 //#region Functions
 function buildCollectionMain(data)
@@ -118,7 +128,7 @@ function buildEntry(asteroid)
     let itemDiv = document.createElement("div");
     itemDiv.classList.add("item");
     itemDiv.addEventListener("click", clickEvent => moveClickedItem(clickEvent));
-
+    //create card heading using lightly edited name of asteroid
     let h2 = document.createElement("h2");
     let astName = asteroid.englishName.split(" ");
     astName.shift();
@@ -133,6 +143,7 @@ function buildEntry(asteroid)
     itemDiv.appendChild(ul);
 
     let mass = asteroid.mass;
+    //not sure why some asteroids have null mass, but it has to be accounted for
     let massString = mass == null ? "[Not Found]" : `${asteroid.mass.massValue}x10^${asteroid.mass.massExponent}`;
     ul.appendChild(buildItemDataRow("Mass:", massString));
     ul.appendChild(buildItemDataRow("Mean Radius:", asteroid.meanRadius));
@@ -158,37 +169,51 @@ function buildItemDataRow(rowLabel, rowValue)
     return li;
 }
 
+function setupSortButtonFor(collection)
+{
+    let sortButton = collection === collectionMain ? collectionMainSort : collectionFavSort;
+    sortButton.addEventListener("click", () => {
+        collection.invertSortSetting();
+        sortCollection(collection);
+    });
+}
+
 function moveClickedItem(clickEvent)
 {
-    let asteroidCard = clickEvent.target;
-    asteroidCard.classList.remove("transition");
-    let prevParent = asteroidCard.parentElement;
+    let clickedCard = clickEvent.target;
+    let prevParent = clickedCard.parentElement;
     let prevParentCollection = prevParent === collectionMain.listDiv ? collectionMain : collectionFavorites;
     let newParentCollection = prevParent === collectionMain.listDiv ? collectionFavorites : collectionMain;
-    let startRect = asteroidCard.getBoundingClientRect();
-    newParentCollection.listDiv.appendChild(asteroidCard)
+    let startRect = clickedCard.getBoundingClientRect(); //get screen position of card BEFORE changing parent
+    newParentCollection.listDiv.appendChild(clickedCard)
     sortCollection(newParentCollection);
-    let astObject = cardObjects.get(asteroidCard);
+    updateCollectionMasses(clickedCard, prevParentCollection, newParentCollection);
+    doCardMoveAnimation(clickedCard, startRect);
+}
+
+function updateCollectionMasses(clickedCard, prevParentCollection, newParentCollection)
+{
+    let astObject = cardObjects.get(clickedCard);
     let astMass = astObject.mass;
     prevParentCollection.addMass(astMass, -1);
     prevParentCollection.updateSumText();
     newParentCollection.addMass(astMass);
     newParentCollection.updateSumText();
-    let endRect = asteroidCard.getBoundingClientRect();
-    let differenceX = startRect.x - endRect.x;
-    let differenceY = startRect.y - endRect.y;
-    asteroidCard.style.transform = `translate(${differenceX}px, ${differenceY}px)`;
-    setTimeout(() => {
-        asteroidCard.classList.add("transition");
-        asteroidCard.style.transform = "translate(0)";
-    }, 1);
 }
 
-function updateSums()
+function doCardMoveAnimation(clickedCard, startRect)
 {
-    let mainSum = collectionMain.asteroids.reduce((accumulator, item) => {
-
-    })
+    let endRect = clickedCard.getBoundingClientRect();
+    let differenceX = startRect.x - endRect.x;
+    let differenceY = startRect.y - endRect.y;
+    //snap card instantly to a specific offset from new parent, so it looks like it hasn't moved yet
+    clickedCard.classList.remove("transition");
+    clickedCard.style.transform = `translate(${differenceX}px, ${differenceY}px)`;
+    //wait 1 millisecond for translate to take effect, then reapply transition class, allowing animation to play
+    setTimeout(() => {
+        clickedCard.classList.add("transition");
+        clickedCard.style.transform = "translate(0)";
+    }, 1);
 }
 
 function sortCollection(targetCollection)
@@ -204,6 +229,7 @@ function sortCollection(targetCollection)
     }
 }
 
+//utility function for alphabetical sorting
 function compareStrings(str1, str2, reverse)
 {
     if (str1.toLowerCase() < str2.toLowerCase()) 
